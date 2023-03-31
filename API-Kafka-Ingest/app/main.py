28d6050d4f43ad59eb93a6c768d3252c71129ea5
AdnenMess <adnen_messaoudi@hotmail.com>
2023-03-31 18:00:49 +0100

    API_kafka_Localhost

--- API-Kafka-Ingest/app/main.py
+++ API-Kafka-Ingest/app/main.py
@@ -1,8 +1,12 @@
+import json
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from schemas import Business, Checkin, Review, Tip, User
 from flatten_json import flatten
+from kafka import KafkaProducer
 
 app = FastAPI()
 
@@ -22,7 +26,10 @@ async def create_business(business: Business):
     # Flatten the object
     flat_business = flatten(business_obj.dict(), separator='.')
 
-    return JSONResponse(content=dict(flat_business), status_code=201)
+    # Send the data to Kafka
+    await send_to_kafka(json.dumps(flat_business))
+
+    return JSONResponse(content=jsonable_encoder(flat_business), status_code=201)
 
 
 @app.post("/checkin")
@@ -31,6 +38,9 @@ async def create_checkin(checkin: Checkin):
 
     checkin = checkin.dict()
     checkin_obj = Checkin(**checkin)
+
+    # Send the data to Kafka
+    await send_to_kafka(json.dumps(checkin))
 
     return JSONResponse(content=dict(checkin_obj), status_code=201)
 
@@ -42,6 +52,9 @@ async def create_review(review: Review):
     review = review.dict()
     review_obj = Review(**review)
 
+    # Send the data to Kafka
+    await send_to_kafka(json.dumps(review))
+
     return JSONResponse(content=dict(review_obj), status_code=201)
 
 
@@ -51,6 +64,9 @@ async def create_review(tip: Tip):
 
     tip = tip.dict()
     tip_obj = Tip(**tip)
+
+    # Send the data to Kafka
+    await send_to_kafka(json.dumps(tip))
 
     return JSONResponse(content=dict(tip_obj), status_code=201)
 
@@ -62,7 +78,35 @@ async def create_review(user: User):
     user = user.dict()
     user_obj = User(**user)
 
+    # Send the data to Kafka
+    await send_to_kafka(json.dumps(user))
+
     return JSONResponse(content=dict(user_obj), status_code=201)
 
-# Start the server on the localhost
+
+async def send_to_kafka(value):
+    # linger_ms=500, producer will wait up to half a second before sending a batch of messages to improve performance
+    # by reducing the number of network round trips required to send messages
+
+    # Create producer if we work on localhost
+    producer = KafkaProducer(
+        bootstrap_servers='localhost:9093',
+        acks=1,
+        linger_ms=500
+    )
+    # Create producer if we work on docker container
+    # producer = KafkaProducer(
+    #     bootstrap_servers='kafka:9092',
+    #     acks=1,
+    #     linger_ms=500
+    # )
+    # Convert JSON string to bytes
+
+    # Send the message to Kafka
+    producer.send("Yelp-topic", bytes(value, 'utf-8'))
+
+    producer.flush()
+
+
+# Start the server of FastAPI on the localhost
 uvicorn.run(app, host="127.0.0.1", port=8000)
